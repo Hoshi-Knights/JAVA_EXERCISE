be0a85f383f20e61b1fb5031899583bd3dbf6d6b
user <user@DESKTOP-AA217J5>
2017-11-07 20:54:07 +0900

    chapter04

---
+++ src/icehs/science/chapter04/AndOrOperation.java
@@ -0,0 +1,20 @@
+package icehs.science.chapter04;
+
+public class AndOrOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=15;
+		int b=16;
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		
+		System.out.println((a<=b)&(a==b));
+		System.out.println((a>=b)&(a==++b));
+		System.out.println(b);
+		System.out.println((a>=b)&&(++a==b));
+		System.out.println(a);
+
+	}
+
+}
